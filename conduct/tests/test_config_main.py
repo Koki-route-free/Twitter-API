0cd7ee14b39548c74dc222897ff8869a688892a8
Koki-route-free <ykouki0112@gmail.com>
2022-01-19 01:15:50 +0900

    master commit

---
+++ conduct/tests/test_config_main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+import main.config_main as CM    
+# 値が代入されているかの確認です    
+def test_config():
+  assert CM.API_KEY
+  assert CM.API_SECRET_KEY
+  assert CM.ACCESS_TOKEN
+  assert CM.ACCESS_TOKEN_SECRET
+  assert CM.BEARER_TOKEN
+  assert CM.TWITTER_ID
